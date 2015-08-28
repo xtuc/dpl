c25adc7849f6f23b5ad327fa807e29df4e2fdb4f
Hiro Asari <asari.ruby@gmail.com>
2015-08-28 12:17:56 -0400

    Remove cf installation instructions from anynines_spec.rb
    This is tested by cloudfoundry_spec.rb

--- spec/provider/anynines_spec.rb
+++ spec/provider/anynines_spec.rb
@@ -11,7 +11,6 @@ describe DPL::Provider::Anynines do
 
   describe "#check_auth" do
     example do
-      expect(provider.context).to receive(:shell).with('wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/latest/cf-cli_amd64.deb -qO temp.deb && sudo dpkg -i temp.deb')
       expect(provider.context).to receive(:shell).with('rm temp.deb')
       expect(provider.context).to receive(:shell).with('cf api https://api.de.a9s.eu')
       expect(provider.context).to receive(:shell).with('cf login --u mallomar --p myreallyawesomepassword --o myorg --s outer')
