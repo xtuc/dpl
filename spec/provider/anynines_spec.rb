84c524af0803e40248e93e845389ffd613549cfc
Hiro Asari <asari.ruby@gmail.com>
2015-08-28 12:28:45 -0400

    Reinstate spec

--- spec/provider/anynines_spec.rb
+++ spec/provider/anynines_spec.rb
@@ -11,6 +11,7 @@ describe DPL::Provider::Anynines do
 
   describe "#check_auth" do
     example do
+      expect(provider.context).to receive(:shell).with('curl -sSL -O http://go-cli.s3-website-us-east-1.amazonaws.com/releases/latest/cf-cli_amd64.deb; ar x cf-cli_amd64.deb && tar xf data.tar.xz --strip-components 3 ./usr/bin/cf; rm -f data.tar.xz')
       expect(provider.context).to receive(:shell).with('./cf api https://api.de.a9s.eu')
       expect(provider.context).to receive(:shell).with('./cf login --u mallomar --p myreallyawesomepassword --o myorg --s outer')
       provider.check_auth
